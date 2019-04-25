e83d1e151ac66736cc8eb5b87cc45a91456cc245
Caridad Rivera <ccrive10@gmail.com>
2019-04-25 14:05:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// document.addEventListener(DOMContentLoaded, function() {
+//   console.log("The DOM has loaded");
+// })
+//
+//
+//
+// //because it is outside of the event listener.
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let textArea = document.querySelector('#text')
+  textArea.innerHTML = "This is really cool!";
+});
