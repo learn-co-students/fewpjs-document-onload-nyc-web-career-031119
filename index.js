2ec06bd4da7841541fe23cdc358c8b330509fbe8
Tommy Lau <tlau425@yahoo.com>
2019-04-25 17:31:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  pTag()
+})
+
+function pTag(){
+   document.getElementById('text')
+  .innerHTML = "This is really cool!"
+}
